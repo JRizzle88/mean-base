b96a36a3699f214da058cdcf3a33e19f469d474a
JRizzle88 <jrizzle88@yahoo.com>
2016-02-06 23:14:27 -0600

    changed mongodb name to something that relates to the starter app - adjusted format in passportjs

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
 	// the database url to connect
-	url : 'mongodb://127.0.0.1:27017/alliance'
+	url : 'mongodb://127.0.0.1:27017/meanbase'
 }
--- config/passport.js
+++ config/passport.js
@@ -32,9 +32,9 @@ module.exports = function(passport) {
   // by default, if there was no name, it would just be called 'local'
   passport.use('local-signup', new LocalStrategy({
     // by default, local strategy uses username and password, we will override with email
-    usernameField : 'email',
-    passwordField : 'password',
-    passReqToCallback : true // allows us to pass back the entire request to the callback
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true // allows us to pass back the entire request to the callback
   },
   function(req, email, password, done) {
     // asynchronous
@@ -54,10 +54,10 @@ module.exports = function(passport) {
 
           // if there is no user with that email
           // create the user
-          var newUser            = new User();
+          var newUser = new User();
 
           // set the user's local credentials
-          newUser.local.email    = email;
+          newUser.local.email = email;
           newUser.local.password = newUser.generateHash(password);
 
           // save the user
@@ -81,15 +81,15 @@ module.exports = function(passport) {
   // by default, if there was no name, it would just be called 'local'
   passport.use('local-login', new LocalStrategy({
     // by default, local strategy uses username and password, we will override with email
-    usernameField : 'email',
-    passwordField : 'password',
-    passReqToCallback : true // allows us to pass back the entire request to the callback
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true // allows us to pass back the entire request to the callback
   },
   function(req, email, password, done) { // callback with email and password from our form
 
     // find a user whose email is the same as the forms email
     // we are checking to see if the user trying to login already exists
-    User.findOne({ 'local.email' :  email }, function(err, user) {
+    User.findOne({ 'local.email':  email }, function(err, user) {
       // if there are any errors, return the error before anything else
       if (err)
       return done(err);
